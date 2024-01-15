be4a7b29e345711cd6c8b6c576bcc65c31b36f8c
Serine Solheim <serine.solheim@gmail.com>
2024-01-15 14:07:22 +0100

    Added colorful joke

---
+++ app.mjs
@@ -0,0 +1,15 @@
+import chalk from "chalk";
+
+console.log(
+	chalk.rgb(138, 80, 76)("I") +
+	" " + 
+	chalk.rgb(138, 108, 76)("invented") +
+	" " + 
+	chalk.rgb(138, 133, 76)("a") +
+	" " + 
+	chalk.rgb(90, 138, 76)("new") +
+	" " + 
+	chalk.rgb(76, 138, 138)("word!") +
+	" " + 
+	chalk.rgb(76, 84, 138)("Plagiarism!")
+);
